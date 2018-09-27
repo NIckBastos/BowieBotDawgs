bdce17bc57bf0ba503ecd85e379d1ba05340b225
Nickbastos <nickbastos4@gmail.com>
2018-09-27 14:42:51 -0500

    Just created this repo and I added our code from the last years code, and this years new code.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BotDawg.java
@@ -0,0 +1,101 @@
+package org.firstinspires.ftc.teamcode;/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * This is NOT an opmode.
+ *
+ * This class can be used to define all the specific hardware for a single robot.
+ * In this case that robot is a Pushbot.
+ * See PushbotTeleopTank_Iterative and others classes starting with "Pushbot" for usage examples.
+ *
+ * This hardware class assumes the following device names have been configured on the robot:
+ * Note:  All names are lower case and some have single spaces between words.
+ *
+ * Motor channel:  Left  drive motor:        "left_drive"
+ * Motor channel:  Right drive motor:        "right_drive"
+ * Motor channel:  Manipulator drive motor:  "left_arm"
+ * Servo channel:  Servo to open left claw:  "left_hand"
+ * Servo channel:  Servo to open right claw: "right_hand"
+ */
+public class BotDawg
+{
+    /* Public OpMode members. */
+    public DcMotor leftFrontMotor   = null;
+    public DcMotor leftBackMotor  = null;
+    public DcMotor rightFrontMotor     = null;
+    public DcMotor rightBackMotor    = null;
+    public DcMotor liftMotor   = null;
+    public Servo leftClampServo    = null;
+    public Servo rightClampServo   = null;
+    public Servo armServo = null;
+    public ColorSensor colorSensor;
+    public static final double MID_SERVO       =  0.5 ;
+    public static final double ARM_UP_POWER    =  0.45 ;
+    public static final double ARM_DOWN_POWER  = -0.45 ;
+
+    /* local OpMode members. */
+    HardwareMap hwMap           =  null;
+    private ElapsedTime period  = new ElapsedTime();
+
+    /* Constructor */
+    public BotDawg(){
+
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap hardwareMap) {
+
+        //Assigning variables
+
+        leftFrontMotor = hardwareMap.dcMotor.get("Leftfront");
+        leftBackMotor = hardwareMap.dcMotor.get("Leftback");
+        rightFrontMotor = hardwareMap.dcMotor.get("Rightfront");
+        rightBackMotor = hardwareMap.dcMotor.get("Rightback");
+        liftMotor = hardwareMap.dcMotor.get("Lift");
+        leftClampServo = hardwareMap.servo.get("LeftClamp");
+        rightClampServo = hardwareMap.servo.get("RightClamp");
+        armServo = hardwareMap.servo.get("ServoArm");
+        colorSensor = hardwareMap.colorSensor.get("ColorSensor");
+
+        //Assigning directions of motors
+
+        leftFrontMotor.setDirection(DcMotor.Direction.REVERSE);
+        leftBackMotor.setDirection(DcMotor.Direction.REVERSE);
+        rightFrontMotor.setDirection(DcMotor.Direction.FORWARD);
+        rightBackMotor.setDirection(DcMotor.Direction.FORWARD);
+        leftFrontMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+ }
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOpMode.java
@@ -0,0 +1,166 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp(name = "MainTeleOpMode", group = "TeleOp")
+
+public class MainTeleOpMode extends OpMode {
+
+    //Creating variables
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+    double leftJoyStick, rightJoyStick, leftMotorPower, rightMotorPower, liftMotorPower, clampMotorPower, leftServoPower, rightServoPower;
+
+    final private static double JOYSTICK_DEADBAND = 0.1;
+
+    //Encoder Ticks Variables
+    static final double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
+    static final double     DRIVE_GEAR_REDUCTION    = 2.0 / 3 ;     // This is < 1.0 if geared UP
+    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
+    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1415);
+    static final double     DRIVE_SPEED             = 0.6;
+    static final double     TURN_SPEED              = 0.5;
+
+    double motorLiftDownwardSpeed = 0.0;
+    double motorLiftUpwardSpeed = 0.0;
+    //Lift Limit varibles
+    //double liftUpdatedTicks;
+    double liftTotalTicks =  COUNTS_PER_INCH * 9;
+
+    double motorMovementMin = 0.0;
+    double motorMovementMax = 0.0;
+
+    BotDawg robot;
+
+    @Override
+    public void init() {
+
+        robot = new BotDawg();
+        robot.init(hardwareMap);
+
+
+        //I'VE SWITCH THE POWER ON THE SERVO SO THAT WHEN THE ROBOT STARTS IT CLOSES THE ARMS TO GRAB THE BLOCK
+        rightServoPower = -0.75;
+        leftServoPower = 0.75;
+
+        if(robot.armServo.getPosition() == 0.0){
+            robot.armServo.setPosition(1.0);
+        }
+
+    }
+
+    //Code that resets the elapsed time once the driver hits play
+    @Override
+    public void start() {
+        runtime.reset();
+
+    }
+
+
+    public void loop() {
+
+        //Assigning gamepad values
+
+        leftJoyStick = -gamepad1.left_stick_y;
+        rightJoyStick = gamepad1.right_stick_x;
+
+
+        // Making a button so that the arm for the jewel can be brought up in a extreme case scenario
+        if (gamepad2.x && !gamepad2.y || gamepad2.a){
+            robot.armServo.setPosition(1.0);
+        }
+        ///This is to select if the driver wants slow mode or fast mode. Pressing gamepad A will use
+        //fast mode while pressing gamepad Y will use slow mode
+
+        if (gamepad1.a && !gamepad1.y){
+            motorMovementMin = -0.4;
+            motorMovementMax = 0.3;
+
+        }else if (gamepad1.y && !gamepad1.a){
+            motorMovementMin = -0.15;
+            motorMovementMax = 0.15;
+        }
+
+        //This is for limiting the speed of the Lift motor if the driver wants to slow it down.
+
+        if (gamepad2.a && !gamepad2.y){
+            motorLiftDownwardSpeed = -1;
+            motorLiftUpwardSpeed = 1;
+
+        }else if (gamepad2.y && !gamepad2.a){
+            motorLiftDownwardSpeed = -0.50;
+            motorLiftUpwardSpeed = 0.50;
+        }
+
+
+        if (gamepad2.dpad_up && !gamepad2.dpad_down) {
+            liftMotorPower = -motorLiftUpwardSpeed;
+        } else if (!gamepad2.dpad_up && gamepad2.dpad_down) {
+            liftMotorPower = -motorLiftDownwardSpeed;
+        } else{
+        liftMotorPower = 0;
+    }
+
+
+
+        //This code cheks to see if either of the triggers are pressed and the corresponding action
+        //is actuated
+        if (gamepad2.right_trigger > 0.5 && gamepad2.left_trigger < 0.3){
+            rightServoPower = -0.9;
+            leftServoPower = .75;
+        } else if (gamepad2.right_trigger < 0.3 && gamepad2.left_trigger > 0.5){
+            rightServoPower = .75;
+            leftServoPower = -.8;
+        } else if (gamepad2.x){
+            rightServoPower = -.25;
+            leftServoPower = .15;
+        }
+
+        //Testing JOYSTICK_DEADBAND
+
+        if (Math.abs(leftJoyStick) < JOYSTICK_DEADBAND) leftJoyStick = 0;
+        if (Math.abs(rightJoyStick) < JOYSTICK_DEADBAND) rightJoyStick = 0;
+
+        //Assiging POV drive values
+
+        leftMotorPower = Range.clip(leftJoyStick + rightJoyStick, motorMovementMin, motorMovementMax);
+        rightMotorPower = Range.clip(leftJoyStick - rightJoyStick, motorMovementMin, motorMovementMax);
+
+        //Assigning power to each servo and clipping clampMotorPower
+
+        clampMotorPower = Range.clip(clampMotorPower, -1.0, 1.0);
+
+        //We use the value selected by the driver and these lines of codes set the power
+        //to the motor
+        robot.leftFrontMotor.setPower(leftMotorPower);
+        robot.leftBackMotor.setPower(leftMotorPower);
+        robot.rightFrontMotor.setPower(rightMotorPower);
+        robot.rightBackMotor.setPower(rightMotorPower);
+        robot.liftMotor.setPower(liftMotorPower);
+
+        robot.leftClampServo.setPosition(leftServoPower);
+        robot.rightClampServo.setPosition(rightServoPower);
+
+
+
+        //Telemetry is not used to control the robot, it is purely to help debug by showing
+        //Information on the phone
+        telemetry.addData("Status", "Run Time: " + runtime.toString());
+        telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftMotorPower, rightMotorPower);
+        telemetry.addData("LeftServoPower", "power: (%.2f)", leftServoPower);
+        telemetry.addData("RightServoPower", "power: (%.2f)", rightServoPower);
+        telemetry.addData("lift", "power: (%.2f)", liftMotorPower);
+        telemetry.addData("Blue", robot.colorSensor.blue());
+        telemetry.addData("Red", robot.colorSensor.red());
+        //telemetry.addData("CurrentPostition", "currentPosition: (%.2f)", liftUpdatedTicks);
+
+    }
+
+
+    //use stop function to go back to bottom position
+}
