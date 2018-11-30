f656c5d1a57af3fae25eacfcec929a34c64d2146
Nicolas Bastos <nickbastos4@gmail.com>
2018-11-30 17:24:50 -0500

    joined our methods with tensor flow. Before testing any numbers. Testing begins

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptTensorFlowObjectDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptTensorFlowObjectDetection.java
@@ -29,11 +29,26 @@
 
 package org.firstinspires.ftc.teamcode;
 
+import android.content.Context;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 import java.util.List;
+import java.util.Locale;
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.Func;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection;
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
@@ -81,11 +96,70 @@ public class ConceptTensorFlowObjectDetection extends LinearOpMode {
      */
     private TFObjectDetector tfod;
 
+    //Our Stuff
+
+    // The IMU sensor object
+    BNO055IMU imu;
+
+    // State used for updating telemetry
+    Orientation angles;
+    Acceleration gravity;
+
+    /* Declare OpMode members. */
+    private BotDawg robot = new BotDawg();   // Use a Pushbot's hardware
+    private ElapsedTime runtime = new ElapsedTime();
+
+    private static final double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
+    private static final double     DRIVE_GEAR_REDUCTION    = 1.0 ;     // This is < 1.0 if geared UP
+    private static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
+    private static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+        (WHEEL_DIAMETER_INCHES * 3.1415);
+    static final double     DRIVE_SPEED             = 0.7;
+    static final double     TURN_SPEED              = 0.5;
+    static final double     Lift_Speed              = 0.4;
 
 
-    @Override
+  @Override
     public void runOpMode() {
-        // The TFObjectDetector uses the camera frames from the VuforiaLocalizer, so we create that
+
+      robot.init(hardwareMap);
+
+      // Send telemetry message to signify robot waiting;
+      telemetry.addData("Status", "Resetting Encoders");
+      telemetry.update();
+      // Send telemetry message to indicate successful Encoder reset
+      telemetry.addData("Path0",  "Starting at %7d :%7d",
+          robot.leftFrontMotor.getCurrentPosition(),
+          robot.rightFrontMotor.getCurrentPosition(),
+          robot.leftBackMotor.getCurrentPosition(),
+          robot.rightBackMotor.getCurrentPosition());
+      telemetry.update();
+
+
+
+      // Set up the parameters with which we will use our IMU. Note that integration
+      // algorithm here just reports accelerations to the logcat log; it doesn't actually
+      // provide positional information.
+      BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+      parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+      parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+      parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+      parameters.loggingEnabled      = true;
+      parameters.loggingTag          = "IMU";
+      parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+
+      // Initialize sensor parameters
+      imu.initialize(parameters);
+
+      // Wait for the game to start (driver presses PLAY)
+      waitForStart();
+      telemetry.addData("Path", "Complete");
+      telemetry.update();
+      Context context = hardwareMap.appContext;
+      imu.startAccelerationIntegration(new Position(), new Velocity(), 1000);
+      waitForStart();
+    // The TFObjectDetector uses the camera frames from the VuforiaLocalizer, so we create that
         // first.
         initVuforia();
 
@@ -129,6 +203,7 @@ public class ConceptTensorFlowObjectDetection extends LinearOpMode {
                         if (goldMineralX != -1 && silverMineral1X != -1 && silverMineral2X != -1) {
                           if (goldMineralX < silverMineral1X && goldMineralX < silverMineral2X) {
                             telemetry.addData("Gold Mineral Position", "Left");
+
                           } else if (goldMineralX > silverMineral1X && goldMineralX > silverMineral2X) {
                             telemetry.addData("Gold Mineral Position", "Right");
                           } else {
@@ -175,4 +250,231 @@ public class ConceptTensorFlowObjectDetection extends LinearOpMode {
         tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
         tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_GOLD_MINERAL, LABEL_SILVER_MINERAL);
     }
+  void composeTelemetry() {
+
+    // At the beginning of each telemetry update, grab a bunch of data
+    // from the IMU that we will then display in separate lines.
+    telemetry.addAction(new Runnable() { @Override public void run()
+    {
+      // Acquiring the angles is relatively expensive; we don't want
+      // to do that in each of the three items that need that info, as that's
+      // three times the necessary expense.
+      angles   = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+      gravity  = imu.getGravity();
+    }
+    });
+
+    telemetry.addLine()
+        .addData("status", new Func<String>() {
+          @Override public String value() {
+            return imu.getSystemStatus().toShortString();
+          }
+        })
+        .addData("calib", new Func<String>() {
+          @Override public String value() {
+            return imu.getCalibrationStatus().toString();
+          }
+        });
+
+    telemetry.addLine()
+        .addData("heading", new Func<String>() {
+          @Override public String value() {
+            return formatAngle(angles.angleUnit, angles.firstAngle);
+          }
+        })
+        .addData("roll", new Func<String>() {
+          @Override public String value() {
+            return formatAngle(angles.angleUnit, angles.secondAngle);
+          }
+        })
+        .addData("pitch", new Func<String>() {
+          @Override public String value() {
+            return formatAngle(angles.angleUnit, angles.thirdAngle);
+          }
+        });
+
+    telemetry.addLine()
+        .addData("grvty", new Func<String>() {
+          @Override public String value() {
+            return gravity.toString();
+          }
+        })
+        .addData("mag", new Func<String>() {
+          @Override public String value() {
+            return String.format(Locale.getDefault(), "%.3f",
+                Math.sqrt(gravity.xAccel*gravity.xAccel
+                    + gravity.yAccel*gravity.yAccel
+                    + gravity.zAccel*gravity.zAccel));
+          }
+        });
+  }
+
+  String formatAngle(AngleUnit angleUnit, double angle) {
+    return formatDegrees(AngleUnit.DEGREES.fromUnit(angleUnit, angle));
+  }
+
+  String formatDegrees(double degrees){
+    return String.format(Locale.getDefault(), "%.1f", AngleUnit.DEGREES.normalize(degrees));
+  }
+
+  public void encoderDrive(double speed,
+      double leftInches, double rightInches,
+      double timeoutS) {
+    int newLeftFrontTarget;
+    int newRightFrontTarget;
+    int newLeftBackTarget;
+    int newRightBackTarget;
+
+    // Ensure that the opmode is still active
+    if (opModeIsActive()) {
+
+      // Determine new target position, and pass to motor controller
+      newLeftFrontTarget = robot.leftFrontMotor.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
+      newRightFrontTarget = robot.rightFrontMotor.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
+      newLeftBackTarget = robot.leftBackMotor.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
+      newRightBackTarget = robot.rightBackMotor.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
+
+      robot.leftFrontMotor.setTargetPosition(newLeftFrontTarget);
+      robot.rightFrontMotor.setTargetPosition(newRightFrontTarget);
+      robot.leftBackMotor.setTargetPosition(newLeftBackTarget);
+      robot.rightBackMotor.setTargetPosition(newRightBackTarget);
+
+      // Turn On RUN_TO_POSITION
+      robot.leftFrontMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+      robot.rightFrontMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+      robot.leftBackMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+      robot.rightBackMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+      // reset the timeout time and start motion.
+      runtime.reset();
+      robot.leftFrontMotor.setPower(Math.abs(speed));
+      robot.rightFrontMotor.setPower(Math.abs(speed));
+      robot.leftBackMotor.setPower(Math.abs(speed));
+      robot.rightBackMotor.setPower(Math.abs(speed));
+
+      // keep looping while we are still active, and there is time left, and both motors are running.
+      // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
+      // its target position, the motion will stop.  This is "safer" in the event that the robot will
+      // always end the motion as soon as possible.
+      // However, if you require that BOTH motors have finished their moves before the robot continues
+      // onto the next step, use (isBusy() || isBusy()) in the loop test.
+      while (opModeIsActive() &&
+          (runtime.seconds() < timeoutS) &&
+          (robot.leftFrontMotor.isBusy() && robot.rightFrontMotor.isBusy())) {
+
+        // Display it for the driver.
+        telemetry.addData("Path2",  "Running at %7d :%7d",
+            robot.leftFrontMotor.getCurrentPosition(),
+            robot.rightFrontMotor.getCurrentPosition());
+        telemetry.update();
+      }
+      // Stop all motion;
+      robot.leftFrontMotor.setPower(0);
+      robot.rightFrontMotor.setPower(0);
+      robot.leftBackMotor.setPower(0);
+      robot.rightBackMotor.setPower(0);
+      // Turn off RUN_TO_POSITION
+      robot.leftFrontMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+      robot.rightFrontMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+      //  sleep(250);   // optional pause after each move
+    }
+  }
+
+  public void encoderLift(double speed,
+      double yInches,
+      double timeoutS) {
+    int newYTarget;
+
+
+    // Ensure that the opmode is still active
+    if (opModeIsActive()) {
+
+      // Determine new target position, and pass to motor controller
+      newYTarget = robot.liftMotor.getCurrentPosition() + (int)(yInches * COUNTS_PER_INCH);
+
+
+      robot.liftMotor.setTargetPosition(newYTarget);
+
+      // Turn On RUN_TO_POSITION
+      robot.liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+
+      // reset the timeout time and start motion.
+      runtime.reset();
+      robot.liftMotor.setPower(Math.abs(speed));
+
+
+      while (opModeIsActive() &&
+          (runtime.seconds() < timeoutS) &&
+          (robot.liftMotor.isBusy())) {
+
+        // Display it for the driver.
+        telemetry.addData("Path2",  "Running at %7d :%7d",
+            robot.liftMotor.getCurrentPosition());
+        telemetry.update();
+      }
+      // Stop all motion;
+      robot.liftMotor.setPower(0);
+
+      // Turn off RUN_TO_POSITION
+      robot.liftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+    }
+  }
+
+  public void turn(double degrees) {
+    double targer = degrees + angles.firstAngle;
+    double direction = Math.signum(degrees);
+    boolean done = false;
+    while (opModeIsActive() && !done ) {
+      telemetry.addData("first angle", angles.firstAngle);
+      telemetry.update();
+      robot.leftFrontMotor.setPower(-direction * TURN_SPEED);
+      robot.rightFrontMotor.setPower(direction * TURN_SPEED);
+      robot.leftBackMotor.setPower(direction * TURN_SPEED);
+      robot.rightBackMotor.setPower(-direction * TURN_SPEED);
+      if(direction < 0){
+        done = angles.firstAngle < targer;
+      }else if(direction > 0) {
+        done = angles.firstAngle > targer;
+      }
+    }
+    robot.leftFrontMotor.setPower(0);
+    robot.rightFrontMotor.setPower(Math.abs(0));
+    robot.leftBackMotor.setPower(Math.abs(0));
+    robot.rightBackMotor.setPower(Math.abs(0));
+  }
+
+
+
+
+  // Look at AutonomousSudo text file for more info abouut these 3 methods
+  public void leftPath(){
+    while(opModeIsActive()) {
+//      encoderLift(Lift_Speed,5,5);
+      turn(-30);
+      encoderDrive(DRIVE_SPEED,10,10,15);
+    }
+  }
+
+  public void centerPath(){
+    while(opModeIsActive()) {
+      encoderDrive(DRIVE_SPEED,10,10,15);
+    }
+  }
+
+  public void rightPath(){
+    while(opModeIsActive()) {
+      turn(30);
+      encoderDrive(DRIVE_SPEED,10,10,15);
+    }
+  }
+
+
+  String format(OpenGLMatrix transformationMatrix) {
+    return (transformationMatrix != null) ? transformationMatrix.formatAsTransform() : "null";
+  }
 }
+
+
